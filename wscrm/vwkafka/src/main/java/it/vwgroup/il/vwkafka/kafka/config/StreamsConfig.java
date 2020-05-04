f3eb16bf235e287247d4be5896def19a9ba556cb
Nicola Gaffurini <nicola.gaffurini@xeos.it>
2020-05-04 18:21:13 +0200

    Delete vecchio servizio

--- wscrm/vwkafka/src/main/java/it/vwgroup/il/vwkafka/kafka/config/StreamsConfig.java
+++
@@ -1,9 +0,0 @@
-package it.vwgroup.il.vwkafka.kafka.config;
-
-import org.springframework.cloud.stream.annotation.EnableBinding;
-
-import it.vwgroup.il.vwkafka.kafka.stream.BPStreams;
-
-@EnableBinding(BPStreams.class)
-public class StreamsConfig {
-}
