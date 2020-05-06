323f0e0ace665d42d586d9f46613aa45b118b2db
vscalvin <valter.scalvini@eng.it>
2020-05-06 17:43:45 +0200

    delete sottocartella

--- vgi-il20/auth-service/src/main/java/it/vwgroup/il/auth/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package it.vwgroup.il.auth.service;
-
-import it.vwgroup.il.auth.domain.User;
-
-public interface UserService {
-
-	void create(User user);
-
-}
--- vgi-il20/notification-service/src/main/java/it/vwgroup/il/notification/service/NotificationService.java
+++
@@ -1,8 +0,0 @@
-package it.vwgroup.il.notification.service;
-
-public interface NotificationService {
-
-	void sendBackupNotifications();
-
-	void sendRemindNotifications();
-}
