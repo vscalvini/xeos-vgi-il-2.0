fafb1cc4ccc0db2919f248eb05e2a2258d3210ed
Christian Novelli <christian.novelli@xeos.it>
2020-05-11 09:27:22 +0200

    commit $MONGODB_PASSWORD to password

--- mongodb/init.sh
+++ mongodb/init.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 if test -z "$MONGODB_PASSWORD"; then
     echo "MONGODB_PASSWORD not defined"
-    exit 1
+#   exit 1
 fi
 
-auth="-u user -p $MONGODB_PASSWORD"
+auth="-u user -p password"
 
 # MONGODB USER CREATION
 (
 echo "setup mongodb auth"
-create_user="if (!db.getUser('user')) { db.createUser({ user: 'user', pwd: '$MONGODB_PASSWORD', roles: [ {role:'readWrite', db:'vgi_il'} ]}) }"
+create_user="if (!db.getUser('user')) { db.createUser({ user: 'user', pwd: 'password', roles: [ {role:'readWrite', db:'vgi_il'} ]}) }"
 until mongo vgi_il --eval "$create_user" || mongo vgi_il $auth --eval "$create_user"; do sleep 5; done
 killall mongod
 sleep 1
